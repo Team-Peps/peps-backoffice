a5e89ebf4f7005df6cc3008b678a3cbca371eb50
augustinde <augustin.1201@outlook.fr>
2025-05-20 21:20:24 +0200

    fix: deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	backendUrl: "https://apipeps.serkox.ovh/api/v1",
-	minioBaseUrl: "https://s3peps.serkox.ovh/",
+	minioBaseUrl: "https://minio.s3.serkox.ovh/",
 	tinyMCEApiKey: "hei5xoxgopi3lcc6u84sy5pz8tzsyzm9srcwv37rtwq1jtw7",
 	production: false
 }
