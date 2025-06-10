c709d6fd38b23950571b7db1d5ab15d08970f632
Augustin D <42963508+augustinde@users.noreply.github.com>
2025-06-10 19:17:36 +0200

    feat: updating match with button (#120)

--- src/app/service/match.service.ts
+++ src/app/service/match.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, NgZone} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Match} from '../model/match';
@@ -10,10 +10,32 @@ import {environment} from '@/environments/environment';
 export class MatchService {
 
 	constructor(
-		private http: HttpClient
+		private http: HttpClient,
+		private ngZone: NgZone
 	) {}
 
 	getAllMatches(): Observable<Record<string, Match[]>> {
 		return this.http.get<Record<string, Match[]>>(`${environment.backendUrl}/match`);
+	}
+
+	updateAndSaveMatches(): Observable<string> {
+		return new Observable<string>((observer) => {
+			const eventSource = new EventSource(`${environment.backendUrl}/match/update-and-save`);
+
+			eventSource.onmessage = (event: MessageEvent) => {
+				this.ngZone.run(() => {
+					observer.next(event.data);
+				})
+			};
+
+			eventSource.onerror = (error: Event) => {
+				this.ngZone.run(() => {
+					observer.error(error);
+				});
+				eventSource.close();
+			};
+
+			return () => eventSource.close();
+		})
 	}
 }
