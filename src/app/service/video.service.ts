b13773e852d5269b8acaa5e46b6949d19cccc870
Augustin D <42963508+augustinde@users.noreply.github.com>
2025-06-10 16:55:23 +0200

    feat: adding miniature on last video (#119)

--- src/app/service/video.service.ts
+++ src/app/service/video.service.ts
@@ -17,11 +17,19 @@ export class VideoService {
 		return this.http.get<Video[]>(`${environment.backendUrl}/last-videos`);
 	}
 
-	createVideo(video: Video): Observable<{ message: string; video: Video }> {
-		return this.http.post<{ message: string; video: Video }>(`${environment.backendUrl}/last-videos`, video);
+	createVideo(video: Video, imageFile: File): Observable<{ message: string; video: Video }> {
+		const formData = new FormData();
+		formData.append('video', new Blob([JSON.stringify(video)], { type: 'application/json' }));
+		formData.append('imageFile', imageFile);
+
+		return this.http.post<{ message: string; video: Video }>(`${environment.backendUrl}/last-videos`, formData);
 	}
 
-	updateVideo(video: Video): Observable<{ message: string; video: Video }> {
-		return this.http.put<{ message: string; video: Video }>(`${environment.backendUrl}/last-videos`, video);
+	updateVideo(video: Video, imageFile: File): Observable<{ message: string; video: Video }> {
+		const formData = new FormData();
+		formData.append('video', new Blob([JSON.stringify(video)], { type: 'application/json' }));
+		formData.append('imageFile', imageFile);
+
+		return this.http.put<{ message: string; video: Video }>(`${environment.backendUrl}/last-videos`, formData);
 	}
 }
